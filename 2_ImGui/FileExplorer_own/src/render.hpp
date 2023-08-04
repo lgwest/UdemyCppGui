14eb26a8f855ebc74bc7778f24307255f8613200
LG West <larsgwest@gmail.com>
2023-08-04 17:47:59 +0200

    finish File Explorer

--- 2_ImGui/FileExplorer_own/src/render.hpp
+++ 2_ImGui/FileExplorer_own/src/render.hpp
@@ -17,9 +17,17 @@ private:
     void DrawActions();
     void DrawFilter();
 
+    void openFileWithDefaultEditor();
+    void renameFilePopup();
+    void deleteFilePopup();
+    bool renameFile(const fs::path &old_path, const fs::path &new_path);
+    bool deleteFile(const fs::path &path);
+
 private:
     fs::path currentPath;
     fs::path selectedEntry;
+    bool renameDialogOpen = false;
+    bool deleteDialogOpen = false;
 };
 
 void render(WindowClass &window_obj);
