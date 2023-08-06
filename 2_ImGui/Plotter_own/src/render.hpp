bdcac5a3d1a4ed3e9881d1d710db1e9444d732b4
LG West <larsgwest@gmail.com>
2023-08-06 20:29:46 +0200

    function plotting

--- 2_ImGui/Plotter_own/src/render.hpp
+++ 2_ImGui/Plotter_own/src/render.hpp
@@ -1,12 +1,37 @@
 #pragma once
 
+#include<array>
 #include <cstdint>
+#include <set>
 #include <string_view>
 
 class WindowClass
 {
 public:
+    constexpr static auto functionNames = 
+        std::array<std::string_view, 3>{"unk", "sin(x)", "cos(x)"};
+
+    enum class Function
+    {
+        NONE,
+        SIN,
+        COS,
+    };
+
+public:
+    WindowClass() : selectedFunctions({}){};
+
     void Draw(std::string_view label);
+
+private:
+    void DrawSelection();
+    void DrawPlot();
+
+    Function functionNameMapping(std::string_view function_name);
+    double evaluateFunction(const Function function, const double x);
+
+public:
+    std::set<Function> selectedFunctions;
 };
 
 void render(WindowClass &window_obj);
