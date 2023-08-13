cf936e40ff2644d791b891c9eb2fc9af58cd2d01
LG West <larsgwest@gmail.com>
2023-08-13 20:51:53 +0200

    begin impl compare

--- 2_ImGui/DiffViewer_own/src/render.cpp
+++ 2_ImGui/DiffViewer_own/src/render.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 #include <fmt/format.h>
 #include <imgui.h>
@@ -42,6 +45,14 @@ void WindowClass::DrawSelection()
     {
         SaveFileContent(filePath2, fileContent2);
     }
+
+    if (ImGui::Button("Compare")) 
+    {
+        fileContent1 = LoadFileContent(filePath1);
+        fileContent2 = LoadFileContent(filePath2);
+
+        CreateDiff();
+    }
 }
 
 void WindowClass::DrawDiffView()
@@ -54,15 +65,62 @@ void WindowClass::DrawStats()
 
 WindowClass::FileContent WindowClass::LoadFileContent(std::string_view file_path)
 {
-    return FileContent();
+    auto content = FileContent{};
+    auto in = std::ifstream(file_path.data());
+
+    if (in.is_open())
+    {
+        auto line = std::string{};
+        while (std::getline(in, line));
+        {
+            content.push_back(line);
+        }
+
+        in.close();
+    }
+
+    return content;
 }
 
 void WindowClass::SaveFileContent(std::string_view file_path, FileContent file_content)
 {
+    auto out = std::ofstream(file_path.data());
+
+    if (out.is_open())
+    {
+        auto line = std::string{};
+        for (const auto &line : file_content) 
+        {
+            out << line << '\n';
+        }
+
+        out.close();
+    }
 }
 
 void WindowClass::CreateDiff()
 {
+    diffResult1.clear();
+    diffResult2.clear();
+
+    const auto max_num_lines = std::max(fileContent1.size(), fileContent2.size());
+
+    for (std::size_t i = 0; i < max_num_lines; ++i)
+    {
+        const auto line1 = i < fileContent1.size() ? fileContent1[1] : "EMPTY";
+        const auto line2 = i < fileContent2.size() ? fileContent2[1] : "EMPTY";
+
+        if (line1 != line2)
+        {
+            diffResult1.push_back(line1);
+            diffResult2.push_back(line2);
+        }
+        else
+        {
+            diffResult1.push_back("");
+            diffResult2.push_back("");
+        }
+    }
 }
 
 
