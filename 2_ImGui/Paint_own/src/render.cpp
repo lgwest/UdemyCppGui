1dc92672d122e0aeedf230c23ccba85d5de77cb5
LG West <larsgwest@gmail.com>
2023-08-20 10:49:15 +0200

    Buttons: Save, Load, Clear

--- 2_ImGui/Paint_own/src/render.cpp
+++ 2_ImGui/Paint_own/src/render.cpp
@@ -19,7 +19,112 @@ void WindowClass::Draw(std::string_view label)
 
     ImGui::Begin(label.data(), nullptr, window_flags);
 
+    DrawMenu();
+    DrawCanvas();
+
     ImGui::End();
+}
+void WindowClass::DrawSavePopup()
+{
+    static char saveFilenameBuffer[256] = "text.txt";
+    const auto esc_pressed = ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
+
+    ImGui::SetNextWindowSize(popUpSize);
+    ImGui::SetNextWindowPos(popUpPos
+        // ImVec2(ImGui::GetIO().DisplaySize.x / 2.0F - popUpSize.x / 2.0F,
+        //         ImGui::GetIO().DisplaySize.y / 2.0F - popUpSize.x / 2.0F)
+    );
+    if (ImGui::BeginPopupModal("Save File", nullptr, popUpFlags))
+    {
+        ImGui::InputText("Filename", filenameBuffer, sizeof(filenameBuffer));
+        if (ImGui::Button("Save", popUpButtonSize)) {
+            SaveToImageFile(filenameBuffer);
+            ImGui::CloseCurrentPopup();
+        }
+        ImGui::SameLine();
+
+        if (ImGui::Button("Cancel", popUpButtonSize) || esc_pressed) {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+void WindowClass::DrawLoadPopup()
+{
+    const auto esc_pressed = ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
+
+    ImGui::SetNextWindowSize(popUpSize);
+    ImGui::SetNextWindowPos( popUpPos
+        // ImVec2(ImGui::GetIO().DisplaySize.x / 2.0F - popUpSize.x / 2.0F,
+        //         ImGui::GetIO().DisplaySize.y / 2.0F - popUpSize.x / 2.0F)
+    );
+    if (ImGui::BeginPopupModal("Load File", nullptr, popUpFlags))
+    {
+        ImGui::InputText("Filename", filenameBuffer, sizeof(filenameBuffer));
+        if (ImGui::Button("Load", popUpButtonSize)) {
+            LoadFromImageFile(filenameBuffer);
+            ImGui::CloseCurrentPopup();
+        }
+        ImGui::SameLine();
+
+        if (ImGui::Button("Cancel", popUpButtonSize) || esc_pressed) {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::EndPopup();    
+    }
+}
+
+void WindowClass::DrawMenu()
+{
+    const auto ctrl_pressed = ImGui::GetIO().KeyCtrl;
+    const auto esc_pressed = ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
+    const auto s_pressed = ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_S));
+    const auto l_pressed = ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_L));
+    
+    if (ImGui::Button("Save") || (ctrl_pressed && s_pressed))
+    {
+        ImGui::OpenPopup("Save File");
+    }
+
+    ImGui::SameLine();
+
+    if (ImGui::Button("Load")  || (ctrl_pressed && l_pressed))
+    {
+        ImGui::OpenPopup("Load File");
+    }
+
+    ImGui::SameLine();
+
+    if (ImGui::Button("Clear"))
+    {
+        ClearCanvas();
+    }
+
+    DrawSavePopup();
+    DrawLoadPopup();
+}
+
+void WindowClass::DrawCanvas()
+{
+}
+
+void WindowClass::DrawColorButtons()
+{
+}
+
+void WindowClass::SaveToImageFile(std::string_view filename)
+{
+}
+
+void WindowClass::LoadFromImageFile(std::string_view filename)
+{
+}
+
+void WindowClass::ClearCanvas()
+{
 }
 
 void render(WindowClass &window_obj)
