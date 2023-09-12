9cd1c27f2c00a923d13d2556b7998ab778f166cc
LG West <larsgwest@gmail.com>
2023-09-12 20:17:30 +0200

    created empty function definitions

--- 2_ImGui/CsvTool_own/src/render.cpp
+++ 2_ImGui/CsvTool_own/src/render.cpp
@@ -22,6 +22,85 @@ void WindowClass::Draw(std::string_view label)
     ImGui::End();
 }
 
+void WindowClass::DrawSizeButtons()
+{
+}
+
+void WindowClass::DrawIoButtons()
+{
+}
+
+void WindowClass::DrawTable()
+{
+}
+
+void WindowClass::DrawSavePopup()
+{
+    const auto esc_pressed = ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
+
+    if (ImGui::BeginPopupModal("Save File", nullptr, popUpFlags))
+    {
+        ImGui::InputText("Filename", filenameBuffer, sizeof(filenameBuffer));
+
+        if (ImGui::Button("Save", popUpButtonSize))
+        {
+            SaveToCsvFile(filenameBuffer);
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Cancel", popUpButtonSize) || esc_pressed)
+        {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+void WindowClass::DrawLoadPopup()
+{
+    const auto esc_pressed = ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
+
+    if (ImGui::BeginPopupModal("Load File", nullptr, popUpFlags))
+    {
+        ImGui::InputText("Filename", filenameBuffer, sizeof(filenameBuffer));
+
+        if (ImGui::Button("Load", popUpButtonSize))
+        {
+            LoadFromCsvFile(filenameBuffer);
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Cancel", popUpButtonSize) || esc_pressed)
+        {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+void WindowClass::DrawValuePopup(const int row, const int col)
+{
+}
+
+void WindowClass::SaveToCsvFile(std::string_view filename)
+{
+}
+
+void WindowClass::LoadFromCsvFile(std::string_view filename)
+{
+}
+
+void WindowClass::SetPopupLayout()
+{
+}
+
+
 void render(WindowClass &window_obj)
 {
     window_obj.Draw("CsvTool[own]");
--- 2_ImGui/CsvTool_own/src/render.hpp
+++ 2_ImGui/CsvTool_own/src/render.hpp
@@ -2,11 +2,53 @@
 
 #include <cstdint>
 #include <string_view>
+#include <vector>
 
 class WindowClass
 {
 public:
+    static constexpr auto popUpFlags = ImGuiWindowFlags_NoResize
+                    | ImGuiWindowFlags_NoMove
+                    | ImGuiWindowFlags_NoCollapse
+                    | ImGuiWindowFlags_NoScrollbar;
+    static constexpr auto popUpSize = ImVec2(300.0F, 100.0F);
+    static constexpr auto popUpButtonSize = ImVec2(120.0F, 0.0F);
+    static constexpr auto popUpPos = ImVec2(1280.0F / 2.0F - popUpSize.x / 2.0F,
+                                            720.0F / 2.0F - popUpSize.y / 2.0F);
+
+public:
+    WindowClass(): numCols(0), numRows(0), data({}), filenameBuffer("test.csv") {};
     void Draw(std::string_view label);
+
+private:
+    void DrawSizeButtons();
+    void DrawIoButtons();
+    void DrawTable();
+
+    void DrawSavePopup();
+    void DrawLoadPopup();
+    void DrawValuePopup(const int row, const int col);
+
+    void SaveToCsvFile(std::string_view filename);
+    void LoadFromCsvFile(std::string_view filename);
+
+    template <typename T>
+    void PlotCellValue(std::string_view formatter, const T value);
+    void SetPopupLayout();
+
+private:
+    int32_t numCols;
+    int32_t numRows;
+    std::vector<std::vector<float>> data;
+
+    char filenameBuffer[256];
+
 };
 
 void render(WindowClass &window_obj);
+
+template <typename T>
+inline void WindowClass::PlotCellValue(std::string_view formatter,
+                                       const T value)
+{
+}
